5ad053802cfbda6f8a3a789716ae95d24bedc26b
Septi <septimiu.ujica@gmail.com>
2026-08-24 18:15:00 +0300

    cache blobs by default

--- internal/web/api_proxy_metrics.go
+++ internal/web/api_proxy_metrics.go
@@ -5,5 +5,5 @@ import "net/http"
 func (w *Web) apiProxyMetrics(rw http.ResponseWriter, r *http.Request) {
 	snapshot := w.proxyMetrics.Snapshot()
 
-	apiJSON(rw, http.StatusOK, newProxyMetricsResponse(snapshot, w.cfg.BlobCache.Enabled))
+	apiJSON(rw, http.StatusOK, newProxyMetricsResponse(snapshot, *w.cfg.BlobCache.Enabled))
 }
